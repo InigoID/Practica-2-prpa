45089e90f1ae171b0dc50cdc9a4b73608abd4f7b
InigoID <124082671+InigoID@users.noreply.github.com>
2023-04-07 21:58:41 +0200

    Update prpa_practica_2.py

--- prpa_practica_2.py
+++ prpa_practica_2.py
@@ -3,89 +3,131 @@ import random
 from multiprocessing import Lock, Condition, Process
 from multiprocessing import Value
 
-
-SOUTH = "south"
-NORTH = "north"
+SOUTH = 1
+NORTH = 0
 
 NCARS = 100
-NPEOPLE = 10
-
+NPED = 10
+TIME_CARS_NORTH = 0.5  # a new car enters each 0.5s
+TIME_CARS_SOUTH = 0.5  # a new car enters each 0.5s
+TIME_PED = 5 # a new pedestrian enters each 5s
+TIME_IN_BRIDGE_CARS = (1, 0.5) # normal 1s, 0.5s
+TIME_IN_BRIDGE_PEDESTRIAN = (30, 10) # normal 1s, 0.5s
 
 class Monitor():
     def __init__(self):
         self.mutex = Lock()
-        self.people_waiting = Condition(self.mutex)
-        self.people_in_bridge = Value('i', 0)
-        self.cars_in_bridge = Value('i', 0)
+        self.north_cars = Value('i', 0)
+        self.south_cars = Value('i', 0)
+        self.waiting_north = Value('i', 0)
+        self.waiting_south = Value('i', 0)
+        self.pedestrians = Value('i', 0)
+        self.waiting_pedestrians = Value('i', 0)
+        self.bridge = Condition(self.mutex)
 
-    def wants_enter(self, direction, is_person=False):
-        self.mutex.acquire()
-        if is_person:
-            while self.cars_in_bridge.value > 0 or self.people_in_bridge.value > 0:
-                self.people_waiting.wait()
-        else:
-            while self.people_in_bridge.value > 0:
-                self.people_waiting.wait()
-        if is_person:
-            self.people_in_bridge.value += 1
-        else:
-            self.cars_in_bridge.value += 1
-        self.mutex.release()
+    def wants_enter_car(self, direction: int) -> None:
+        with self.bridge:
+            if direction == NORTH:
+                self.waiting_north.value += 1
+                self.bridge.wait_for(lambda: self.south_cars.value == 0 and self.pedestrians.value == 0)
+                self.waiting_north.value -= 1
+                self.north_cars.value += 1
+            else:
+                self.waiting_south.value += 1
+                self.bridge.wait_for(lambda: self.north_cars.value == 0 and self.pedestrians.value == 0)
+                self.waiting_south.value -= 1
+                self.south_cars.value += 1
 
-    def leaves_bridge(self, direction, is_person=False):
-        self.mutex.acquire()
-        if is_person:
-            self.people_in_bridge.value -= 1
-        else:
-            self.cars_in_bridge.value -= 1
-        self.people_waiting.notify()
-        self.mutex.release()
+    def leaves_car(self, direction: int) -> None:
+        with self.bridge:
+            if direction == NORTH:
+                self.north_cars.value -= 1
+                if self.north_cars.value == 0:
+                    self.bridge.notify_all()
+            else:
+                self.south_cars.value -= 1
+                if self.south_cars.value == 0:
+                    self.bridge.notify_all()
 
+    def wants_enter_pedestrian(self) -> None:
+        with self.bridge:
+            self.waiting_pedestrians.value += 1
+            self.bridge.wait_for(lambda: self.north_cars.value == 0 and self.south_cars.value == 0)
+            self.waiting_pedestrians.value -= 1
+            self.pedestrians.value += 1
 
-def delay(n=3):
-    time.sleep(random.random()*n)
+    def leaves_pedestrian(self) -> None:
+        with self.bridge:
+            self.pedestrians.value -= 1
+            self.bridge.notify_all()
 
+def delay_car_north() -> None:
+    time.sleep(random.uniform(TIME_IN_BRIDGE_CARS[0], TIME_IN_BRIDGE_CARS[1]))
 
-def car(cid, direction, monitor):
-    print(f"car {cid} direction {direction} created")
-    delay(6)
-    print(f"car {cid} heading {direction} wants to enter")
-    monitor.wants_enter(direction)
-    print(f"car {cid} heading {direction} enters the bridge")
-    delay(3)
-    print(f"car {cid} heading {direction} leaving the bridge")
-    monitor.leaves_bridge(direction)
-    print(f"car {cid} heading {direction} out of the bridge")
+def delay_car_south() -> None:
+    time.sleep(random.uniform(TIME_IN_BRIDGE_CARS[0], TIME_IN_BRIDGE_CARS[1]))
 
+def delay_pedestrian() -> None:
+    time.sleep(random.uniform(TIME_IN_BRIDGE_PEDESTRIAN[0], TIME_IN_BRIDGE_PEDESTRIAN[1]))
 
-def person(pid, monitor):
-    print(f"person {pid} created")
-    delay(6)
-    print(f"person {pid} wants to enter")
-    monitor.wants_enter(NORTH, is_person=True)
-    print(f"person {pid} enters the bridge")
-    monitor.leaves_bridge(NORTH, is_person=True)
-    print(f"person {pid} out of the bridge")
+def car(cid: int, direction: int, monitor: Monitor) -> None:
+    print(f"car {cid} heading {direction} wants to enter.")
+    monitor.wants_enter_car(direction)
+    print(f"car {cid} heading {direction} enters the bridge.")
+    if direction == NORTH:
+        delay_car_north()
+    else:
+        delay_car_south()
+    print(f"car {cid} heading {direction} leaving the bridge.")
+    monitor.leaves_car(direction)
+    print(f"car {cid} heading {direction} out of the bridge.")
 
+def pedestrian(pid: int, monitor: Monitor) -> None:
+    print(f"pedestrian {pid} wants to enter.")
+    monitor.wants_enter_pedestrian()
+    print(f"pedestrian {pid} enters the bridge.")
+    delay_pedestrian()
+    print(f"pedestrian {pid} leaving the bridge.")
+    monitor.leaves_pedestrian()
+    print(f"pedestrian {pid} out of the bridge.")
+
+def gen_pedestrian(monitor: Monitor) -> None:
+    pid = 0
+    plst = []
+    for _ in range(NPED):
+        pid += 1
+        p = Process(target=pedestrian, args=(pid, monitor))
+        p.start()
+        plst.append(p)
+        time.sleep(random.expovariate(1 / TIME_PED))
+
+    for p in plst:
+        p.join()
+
+def gen_cars(direction: int, time_cars, monitor: Monitor) -> None:
+    cid = 0
+    plst = []
+    for _ in range(NCARS):
+        cid += 1
+        p = Process(target=car, args=(cid, direction, monitor))
+        p.start()
+        plst.append(p)
+        time.sleep(random.expovariate(1 / time_cars))
+
+    for p in plst:
+        p.join()
 
 def main():
     monitor = Monitor()
-    processes = []
-    for i in range(NCARS):
-        direction = NORTH if random.randint(0, 1) == 1 else SOUTH
-        p = Process(target=car, args=(i+1, direction, monitor))
-        processes.append(p)
-    for i in range(NPEOPLE):
-        p = Process(target=person, args=(i+1, monitor))
-        processes.append(p)
-    random.shuffle(processes)
-    for p in processes:
-        p.start()
-        time.sleep(random.expovariate(1/0.5))
-    for p in processes:
-        p.join()
-    print("simulation ended")
-
+    gcars_north = Process(target=gen_cars, args=(NORTH, TIME_CARS_NORTH, monitor))
+    gcars_south = Process(target=gen_cars, args=(SOUTH, TIME_CARS_SOUTH, monitor))
+    gped = Process(target=gen_pedestrian, args=(monitor,))
+    gcars_north.start()
+    gcars_south.start()
+    gped.start()
+    gcars_north.join()
+    gcars_south.join()
+    gped.join()
 
 if __name__ == '__main__':
     main()
